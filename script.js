99bb88796da0dd153b5c2a293f6b699ec836c53a
itsjzt <saurabh.19@outlook.com>
2018-01-17 22:19:36 +0530

    design finished

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 const $ = e => document.querySelector(e)
 const $$ = e => document.querySelectorAll(e)
 
-const play = (e) => e.target.innerHTML = 'X'
-
 let player = 0
+
+const play = (e) => {
+  // skip if the box is already filled
+  if (e.target.innerHTML) return ;
+  e.target.innerHTML = player ? 'X' : '0'
+  // change the player
+  player = player ? 0 : 1
+  $('#turn').innerHTML = `Player ${player} it's your turn. &curarr;`
+}
+
 
 $$('#hole').forEach(element => {
   element.addEventListener('click', play)
